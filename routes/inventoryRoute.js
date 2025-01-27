623849cc72d68d336eb6c2127e4a0c68452c46ab
BraxtonGoode <tjbgoode@gmail.com>
2025-01-27 11:42:09 -0700

    test 1

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,5 +5,5 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 // Route to build view by inventory Make
-router.get("/:classificationId/:invId", invController.buildByInvId);
+router.get("/detail/:invId", invController.buildByInvId);
 module.exports = router;
