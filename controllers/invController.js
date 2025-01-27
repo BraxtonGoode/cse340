238577d46800d183f1439f437ef14ebc24ba01a9
BraxtonGoode <tjbgoode@gmail.com>
2025-01-27 10:33:11 -0700

    capital I on item

--- controllers/invController.js
+++ controllers/invController.js
@@ -31,6 +31,7 @@ invCont.buildByInvId = async function (req, res, next) {
     const vYear = data[0].inv_year
     const vMake = data[0].inv_make
     const vModel = data[0].inv_model
+    // think isssue was capital I on file
     res.render("./inventory/item", {
       title: `${vYear} ${vMake} ${vModel}`,
       nav,
